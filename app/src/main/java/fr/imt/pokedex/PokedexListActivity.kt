ecbb2767ec68543a2743b0982f9f6f9d464e5206
Rory <rory.maillard@imt-atlantique.net>
2023-03-08 10:32:51 +0100

    Added feature to filter Pokemon list by name

--- app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
+++ app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
@@ -1,11 +1,13 @@
 package fr.imt.pokedex
-
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import fr.imt.pokedex.adapter.PokemonListAdapter
 import fr.imt.pokedex.data.Datasource
@@ -21,7 +23,25 @@ class PokedexListActivity: AppCompatActivity() {
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
         val dataset: List<Pokemon> = Datasource().loadPokemons()
-        recyclerView.adapter = PokemonListAdapter(this, dataset)
+        val editText = findViewById<EditText>(R.id.pokedex_list_view_filter_text)
+        val imageButton =
+            findViewById<ImageButton>(R.id.pokedex_list_view_filter_clear)
+        imageButton.setOnClickListener { editText.setText("") }
+        editText.doOnTextChanged {searchText, _, _, _ ->
+            if (searchText != null) {
+                refreshList(
+                    recyclerView,
+                    dataset.filter { pokemon -> pokemon.name.contains(searchText,
+                        true) })
+            } else {
+                refreshList(recyclerView, dataset)
+            }
+        }
+        refreshList(recyclerView, dataset)
+    }
+    private fun refreshList(recyclerView: RecyclerView, filteredDataSet:
+    List<Pokemon>) {
+        recyclerView.adapter = PokemonListAdapter(this, filteredDataSet)
     }
     companion object {
         fun getAssetsDrawable(context: Context, fileName: String): BitmapDrawable? {
